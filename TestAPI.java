d78a078cedfee10e818da53875ae0a168e1e9e64
SamanAttar <saman.attar@outlook.com>
2017-11-07 16:20:53 -0600

    Update TestAPI.java

--- TestAPI.java
+++ TestAPI.java
@@ -10,24 +10,25 @@ import com.google.gson.JsonParser;
 public class TestAPI
 {
 	public static void main(String[] args) {
-		startWebRequest("Austin");	
+		startWebRequest("Austin");
 	}
+	
 	static String startWebRequest(String city){
 		String weatherURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=26aa1d90a24c98fad4beaac70ddbf274";
-		StringBuilder result = new StringBuilder();
+		StringBuilder result = new StringBuilder(); //will hold the java String after converting from JSON 
 		try {	
-			URL url = new URL(weatherURL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			URL url = new URL(weatherURL); //the url we want to parse
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection(); //Used to make a single connection to that URL
+			conn.setRequestMethod("GET"); //The Type of request we want to make
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream())); //pass in the instance of HttpURLConnection 
 			String line;
-		    	while ((line = rd.readLine()) != null) {
+		    	while ((line = rd.readLine()) != null) { //turn BufferedReader value into type String
 		    		result.append(line);
 		    	}
 		    rd.close();
-	
-		    double longitude = parseJson(result.toString());
-		    System.out.println("Longitude is: " + longitude);
+		    //what do we want to search the API for?
+		    double windGust = parseJson(result.toString());
+		    System.out.println("Wind gust is currently: " + windGust + " mph");
 		    return result.toString();
 		}
 		catch(Exception e){return "Error! Exception: " + e;}
@@ -36,14 +37,10 @@ public class TestAPI
 	static double parseJson(String json) {
         JsonElement jelement = new JsonParser().parse(json);
         JsonObject  MasterWeatherObject = jelement.getAsJsonObject();
+    
+        JsonObject windObject = MasterWeatherObject.getAsJsonObject("wind"); //type this exactly as it appears in the JSON response
+        double gust = windObject.get("gust").getAsDouble();  //type exactly, type case matters!
         
-        JsonObject  coordinateObject = MasterWeatherObject.getAsJsonObject("coord");
-        JsonObject sunsetObject = MasterWeatherObject.getAsJsonObject("sunset");
-        double longitude = coordinateObject.get("lon").getAsDouble();
-        double latitude = sunsetObject.get("sunset").getAsDouble();
-        
-        System.out.println("LAT: " + latitude);
-        
-return longitude;
-}
+	return gust;
+	}
 }
